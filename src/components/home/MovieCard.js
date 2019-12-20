83b34b7ad350bf08ef5931f9ea7f25525647a30c
Jan <jan.andrade@capgemini.com>
2019-12-20 14:24:39 -0300

    fix: cards width correction

--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -4,7 +4,7 @@ export class MovieCard extends Component {
     render() {
         const {movie} = this.props;
         return (
-            <div>
+        
                 <div className="col-md-3 mb-5">
                         <div className="card card-body bg-dark text-center h-100">
                                 <img className="w-100 mb-2" src={movie.Poster} alt="Movie Cover"></img>
@@ -15,7 +15,7 @@ export class MovieCard extends Component {
                                 </a>
                         </div>
                 </div>
-            </div>
+
         )
     }
 }
